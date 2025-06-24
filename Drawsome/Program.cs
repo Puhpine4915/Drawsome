0ecb6581c82afa3d656020972b7958d2213724c1
Puhpine4915 <103285406+Puhpine4915@users.noreply.github.com>
2025-06-24 09:56:23 +0200

    add security for cookies and DDoS

--- Drawsome/Program.cs
+++ Drawsome/Program.cs
@@ -1,3 +1,4 @@
+using System.Threading.RateLimiting;
 using Drawsome.Data;
 using Microsoft.EntityFrameworkCore;
 using Drawsome.Hubs;
@@ -10,6 +11,28 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddSession();
 builder.Services.AddSignalR();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    options.Cookie.SameSite = SameSiteMode.Strict;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+    options.SlidingExpiration = true;
+});
+
+// rate limiting
+builder.Services.AddRateLimiter(options =>
+{
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
+        RateLimitPartition.GetFixedWindowLimiter("Global", _ =>
+            new FixedWindowRateLimiterOptions
+            {
+                PermitLimit = 100,
+                Window = TimeSpan.FromMinutes(1)
+            }));
+});
+
 
 var app = builder.Build();
 
