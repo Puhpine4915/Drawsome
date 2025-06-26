056b0a818e358fe29c47c915d2fc753a849083ea
Puhpine4915 <103285406+Puhpine4915@users.noreply.github.com>
2025-06-26 09:31:06 +0200

    make database local

--- Drawsome/Program.cs
+++ Drawsome/Program.cs
@@ -36,6 +36,22 @@ builder.Services.AddRateLimiter(options =>
 
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
+    context.Response.Headers.Append("X-Frame-Options", "SAMEORIGIN");
+    context.Response.Headers.Append("X-XSS-Protection", "1; mode=block");
+    context.Response.Headers.Append("Referrer-Policy", "strict-origin-always");
+    context.Response.Headers.Append("Content-Security-Policy", 
+        "default-src 'self'; " + 
+        "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
+        "style-src 'self' 'unsafe-inline'; " +
+        "img-src 'self' data:; " +
+        "connect-src 'self';");
+    await next();
+});
+
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
